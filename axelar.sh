ef5e9b295ba5413a15f912fd76c1cb6ce53a4a8d
quangvu-beep <141025169+quangvu-beep@users.noreply.github.com>
2024-05-18 16:11:59 +0700

    axelar.sh

--- axelar.sh
+++ axelar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://tm.axelar.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://tm.axelar.lava.build/lava-referer-804e3634-9706-43f4-8614-52367e17abf3/"
 
 OUTPUT_DIR="logs"
 
